e98df516469d13246be8ead2ca724432ebb6d721
Yasmine <yasmineharfouche0@gmail.com>
2025-09-13 10:45:57 +0100

    Add files via upload

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
